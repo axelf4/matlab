cb59795f95caf5a08ec7a4afb23ef2f77a3b01fa
Axel Forsman <axelsfor@gmail.com>
2019-09-15 21:56:48 +0200

    Add start of part two of the second assignment

--- tms150/lab2/lab2.R
+++ tms150/lab2/lab2.R
@@ -2,21 +2,50 @@
 howell <- read.table("Howell.txt")
 # Includes height (cm), weight (kg), age (years) and gender (1 for male, 0 otherwise)
 attach(howell)
+D <- howell[c("height", "age")]
 N <- nrow(howell) # Number of subjects
+n <- floor(0.7 * N) # Number of rows for the training data
 
-set.seed(123)
+doPolyReg <- function(D) {
+	# Sample training and testing data
+	trainIndices <- sample.int(N, n)
+	Dtrain <- D[trainIndices,]; Dtest <- D[-trainIndices,]
+
+	p <- 1:6
+	pMSE <- sapply(p, function(p) {
+		# Fit yTrain using a polynomial of order p based on xTrain
+		m <- lm(height ~ poly(age, p, raw = TRUE), data = Dtrain)
+		# Make predictions based on xTest covariates
+		pMSE <- mean((Dtest[["height"]] - predict(m, Dtest["age"]))^2)
+})
+	list(p = p, pMSE = pMSE)
+}
+
+# Given the polynomial regression from doPolyReg returns the best p.
+getBestP <- function(reg) {
+	which.min(reg$pMSE + reg$p * 50)
+}
+
+# Replicates `expr`, `n` times, then folds using the function `f` and the initial value.
+foldRep <- function(n, expr, f, init) {
+	for (i in 1:n) init <- f(init, eval.parent(substitute(expr)))
+	init
+}
+
+# Simple forward pipe
+`%>%` <- function(x, f) f(x)
 
 # i) pMSE for polynomial regression with response height and covariate age, p=1:6
-D <- howell[c("height", "age")]
-n <- floor(0.7 * N)
-trainIndices <- sample.int(N, n)
-Dtrain <- D[trainIndices,]; Dtest <- D[-trainIndices,]
+set.seed(123)
+out <- doPolyReg(D)
+plot(out$p, out$pMSE, type = "o")
 
-p <- 1:6
-pMSE <- lapply(p, function(p) {
-	# Fit yTrain using a polynomial of order p based on xTrain
-	m <- lm(height ~ poly(age, p, raw = TRUE), data = Dtrain)
-	# Make predictions based on xTest covariates
-	pMSE <- mean((Dtest[["height"]] - predict(m, Dtest["age"]))^2)
-})
-plot(p, pMSE, type = "o")
+# ii)
+set.seed(123)
+
+foldRep(
+	n = 20,
+	expr = doPolyReg(D) %>% getBestP,
+	f = function(a,x) {a[[x]] <- a[[x]] + 1; a},
+	init = integer(6)
+)
