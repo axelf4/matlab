6a8b58aeb8712cfd14378eb4a6cd3e9eb16510ea
Axel Forsman <axelsfor@gmail.com>
2019-09-30 17:56:38 +0200

    Add start of TMS150 lab 3

---
+++ tms150/lab3.R
@@ -0,0 +1,31 @@
+#     ╭──T_1──╮
+#     │       │
+# A ──┼──T_2──┼──T_4── B
+#     │       │
+#     ╰──T_3──╯
+
+# 1. Compute expected life length of network in figure 6
+lambda_1 = function(t) 1 / (2 * sqrt(t)); lambda_4 = function(t) t^(11/10) / 50
+S_1 = function(t) exp(-sqrt(t)); S_4 = function(t) exp(-t^(21/10) / 105)
+
+# S_1 = function(t) exp(-integrate(lambda_1, 0, t)); S_4 = function(t) exp(-integrate(lambda_4, 0, t))
+
+S = function(t) (1 - (1 - S_1(t))^3) * S_4(t)
+
+expectedLifeLength = integrate(S, 0, Inf)
+expectedLifeLength
+
+# 2. Plot the hazard function lambda_T(t)
+Lambda = deriv(as.expression(do.call('substitute', list(
+		quote(-log((1 - (1 - S_1)^3) * S_4)),
+		list(
+			S_1 = quote(exp(-sqrt(t))),
+			S_4 = quote(exp(-t^(21/10) / 105))
+		)))), "t", function.arg = TRUE)
+lambda = function(t) attr(Lambda(t), "gradient")
+x <- seq(0, 6, length = 200)
+plot(x, lambda(x), 'l')
+
+# 3. Compute the probability that network failure is caused by the failure of T_4
+# Pr(T = T_4) = Pr(T_4 < T_I) = int_0^Inf f_{T_I}(t) * S_{T_4}(t) dt
+# f_T = - S_T'
