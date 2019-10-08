cf4f7168144e33235c80f6b92022ac41484b2c4d
Axel Forsman <axelsfor@gmail.com>
2019-10-08 21:33:05 +0200

    Add most of code for lab 4

--- tms150/lab4/lab4.R
+++ tms150/lab4/lab4.R
@@ -1,28 +1,17 @@
 # Assignment 1
-# b)
+# b) Optimize weight for the case with the two stocks
 data <- read.csv2("stockdata.txt")
 attach(data)
 
-# Utility function for positive x.
-#
-# k != 0 is some parameter and K is the amount of money invested.
-u <- function(x, k, K) (1 - (x / K)^(-k)) / k
-
 # XXX: These are transposed compared to the lecture notes
-X <- cbind(Comp1, Comp2) # X_ji is the price of stock i, after day j
-Z <- diff(log(X)) # Z_ji = log(X_ij / X_i,j-1)
-
-num_stocks <- ncol(X) # Number of stocks
-
-# Estimate parameters from data
-gamma <- apply(Z, 2, mean); Sigma <- var(Z)
+X <- cbind(Comp1, Comp2) # X_ji is the price of stock i after day j
+Z <- diff(log(X)) # Z_ji := log(X_ij / X_i,j-1)
+gamma <- apply(Z, 2, mean); Sigma <- var(Z) # Estimate parameters from data
 
 optimizeWeights <- function(k) {
 	result <- optimize(function(w_1) {
 		w <- c(w_1, 1 - w_1) # Σw_i = 1 => w_2 = 1 - w_1
-		n <- num_stocks # With n = k
-
-		n * crossprod(w, gamma) - k * n * crossprod(w, Sigma %*% w) / 2
+		crossprod(w, gamma) - k * crossprod(w, Sigma %*% w) / 2
 	}, interval = c(0, 1), maximum = TRUE)
 	result$maximum
 }
@@ -33,47 +22,79 @@ barplot(w_1, main = "Amount that should be invested in Comp1 given the value of 
 	xlab = "$k$", ylab = "$w_1$",
 	names.arg = k,
 	ylim = 0:1)
-# See that w_1 ratio decreases with k => Comp1 is more instable
-# We also have that: var(Comp1) > var(Comp2)
 
 # Assignment 2
-# a)
-matureKnee <- read.table("matureKnee.txt")
-immatureKnee <- read.table("immatureKnee.txt")
+# a) Fit logistic regression by numerical ML for the data given parameters a, b
+matureKnee <- read.table("matureKnee.txt"); immatureKnee <- read.table("immatureKnee.txt")
 
-f_1 <- function(x, a, b) exp(a + b * (x - 18)) / (1 + exp(a + b * (x - 18)))
+f_1 <- function(x, a, b) 1 - 1 / (1 + exp(a + b * (x - 18)))
 f_0 <- function(...) 1 - f_1(...)
 
 # Likelihood function
 L <- function(a = 1, b = 1) prod(f_1(matureKnee, a, b)) * prod(f_0(immatureKnee, a, b))
-l <- function(a = 1, b = 1) {
-	sum(log(f_1(matureKnee, a, b))) + sum(log(f_0(immatureKnee, a, b)))
-}
+l <- function(a = 1, b = 1) sum(log(f_1(matureKnee, a, b))) + sum(log(f_0(immatureKnee, a, b)))
 
-res <- optim(par = formals(L), fn = L,
-	control = list(fnscale = -1) # Maximize instead of minimizing
+res <- optim(par = formals(l), fn = l,
+	control = list(fnscale = -1) # Maximize instead of minimize
 )
 a <- res$par[["a"]]; b <- res$par[["b"]]
 
-# Plot the fucker
+# Plot the data together with the logistic regression curve
 x <- seq(0, 40, length.out = 500)
 plot(x, f_1(x, a, b), 'l')
 points(x = matureKnee$V1, y = sapply(matureKnee$V1, function(x) 1))
 points(x = immatureKnee$V1, y = sapply(immatureKnee$V1, function(x) 0))
 
+# b) With a uniform prior, plot the posterior on a grid with values of a and b
+a_min <- -0.4; a_max <- 2.1; b_min <- 0.9; b_max <- 3.1; grid_length <- 21
+A <- seq(a_min, a_max, length.out = grid_length); B <- seq(b_min, b_max, length.out = grid_length)
+
 normalize <- function(a) a / sum(a)
 
-a_min = -0.4; a_max = 2.1; b_min = 0.9; b_max = 3.1
-a <- seq(a_min, a_max, length.out = 100); b <- seq(b_min, b_max, length.out = 100)
-# Uniform prior
-prior = outer(a, b, FUN = Vectorize(function(a, b) 1 / (100 * 100)))
-likelihood <- outer(a, b, FUN = Vectorize(L))
+prior <- outer(A, B, FUN = Vectorize(function(A, B) 1 / grid_length^2)) # Uniform prior
+likelihood <- outer(A, B, FUN = Vectorize(L))
 posterior <- normalize(prior * likelihood)
 
 par(mfrow = c(1, 3), pty="s")
 for (grid in list(list(prior, "Prior"), list(likelihood, "Likelihood"), list(posterior, "Posterior"))) {
-	image(a, b, grid[[1]], xlab = "a", ylab = "b",
+	image(A, B, grid[[1]], xlab = "a", ylab = "b",
 		main = grid[[2]])
 }
 
-# c)
+# c) Find μ, α in the distribution of true ages so that the optimal decision is to classify
+# persons with mature knees as adults
+local({
+	B <- 10
+	c_1 <<- function(x) ifelse(x <= 18, B, 1)
+	c_2 <<- function(x) ifelse(x <= 18, B * (18 - x), x - 18)
+})
+
+# Density function of the distribution of true ages.
+dtrueages <- function(x, mu, alpha) dgamma(x - 14, alpha, alpha / (mu - 14))
+
+mu_min <- 15; mu_max <- 25; alpha_min <- 2; alpha_max <- 9; grid_length <- 31
+Mu <- seq(mu_min, mu_max, length.out = grid_length);
+Alpha <- seq(alpha_min, alpha_max, length.out = grid_length)
+
+opt_mature_adults <- outer(Mu, Alpha, FUN = Vectorize(function(mu, alpha) {
+		f <- function(x) dtrueages(x, mu, alpha) * f_1(x, a, b) * c_1(x)
+		C_c <- integrate(f, lower = 18, upper = Inf)$value
+		C_a <- integrate(f, lower = 0, upper = 18)$value
+		C_c > C_a # Whether optimal to classify as adult
+}))
+image(Mu, Alpha, opt_mature_adults) # Red is TRUE
+
+# d) Same as c) but sum over the uncertainty in the parameters a and b
+opt_mature_adults2 <- outer(Mu, Alpha, FUN = Vectorize(function(mu, alpha) {
+		integrate_out_ab_comp_C <- function(lower, upper) {
+			abmap <- outer(A, B, FUN = Vectorize(function(a, b) {
+					f <- function(x) dtrueages(x, mu, alpha) * f_1(x, a, b) * c_1(x)
+					integrate(f, lower, upper)$value
+			}))
+			sum(posterior * abmap)
+		}
+		C_c <- integrate_out_ab_comp_C(lower = 18, upper = Inf)
+		C_a <- integrate_out_ab_comp_C(lower = 0, upper = 18)
+		C_c > C_a # Whether optimal to classify as adult
+}))
+image(Mu, Alpha, opt_mature_adults2) # Red is TRUE
