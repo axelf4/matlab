9fc49d7c8e17b004b28a358dccbfdc357642c6dc
Axel Forsman <axelsfor@gmail.com>
2019-10-25 14:12:18 +0200

    Add draft of lab 6 report

--- tms150/lab6/lab6.py
+++ tms150/lab6/lab6.py
@@ -1,8 +1,11 @@
 from random import normalvariate
 from itertools import chain, accumulate, filterfalse, repeat, islice, tee
-from matplotlib import pyplot as plt
 from math import exp, sqrt
+import matplotlib
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_pgf import FigureCanvasPgf
 
+matplotlib.backend_bases.register_backend('pdf', FigureCanvasPgf)
 μ = 1; σ = 1
 T = 1 # Endpoint of the time interval
 H = [1 / 2**i for i in range(1, 11)] # List of all values of h
@@ -25,8 +28,7 @@ def pairwise(iterable):
 
 def gen_W_10():
     """Return lists t, W for the case of h = 1 / 2**10."""
-    N = 2**10 # Number of intervals
-    h = 1 / N
+    N = 2**10; h = 1 / N
     η = (normalvariate(0, sqrt(h)) for _ in repeat(0))
     W = list(islice(chain((0,), accumulate(η)), N + 1))
     t = [n * h for n, w in enumerate(W)]
@@ -37,13 +39,13 @@ fig, ax = plt.subplots()
 t, W = gen_W_10()
 for i in range(1, 11):
     plt.plot(list(coarsify(t, i)), list(coarsify(W, i)))
-ax.set(xlabel = 't', ylabel = 'W(t)')
+ax.set(xlabel = '$t$', ylabel = '$W(t)$')
 plt.title('Sample path of a Brownian motion at different resolutions')
 plt.show()
 
 # 2.
 X = lambda t, W: (exp((μ - σ**2 / 2) * t + σ * w) for t, w in zip(t, W))
-plt.plot(t, list(X(t, W)), label = 'X')
+plt.plot(t, list(X(t, W)), label = '$X$')
 
 def X_h(W, i):
     h = 1 / 2**i
@@ -58,9 +60,9 @@ def X_h(W, i):
         X = (1 + h * μ) * X + σ * X * (w_cur - w_prev)
 
 for i in range(1, 11):
-    plt.plot(list(coarsify(t, i)), list(X_h(W, i)), label = f'X_h, i = {i}')
+    plt.plot(list(coarsify(t, i)), list(X_h(W, i)), label = f'$X_h, i = {i}$')
 plt.legend()
-plt.title('Sample paths of X and X_h')
+plt.title('Sample paths of $X$ and $X_h$')
 plt.show()
 
 # 3. Estimate the strong error with MC
@@ -71,21 +73,22 @@ def diff_X_and_X_h(i):
     t, W = gen_W_10()
     return last(X(t, W)) - last(X_h(W, i))
 
-plt.loglog(H, [sqrt(h) for h in H], label = 'Reference slope h^{1/2}')
+plt.loglog(H, [sqrt(h) for h in H], label = 'Reference slope $h^{1/2}$')
 strong_errors = [sqrt(sum(diff_X_and_X_h(i)**2 for _ in range(M)) / M)
         for i in range(1, 11)]
-plt.loglog(H, strong_errors, label = 'Monte Carlo estimates')
+plt.loglog(H, strong_errors, label = 'Strong error Monte Carlo estimate')
 plt.legend()
-plt.title('Strong error for different h')
+plt.title('Strong error for different $h$')
 plt.show()
 
 # 4. Estimate the weak error of |E[X(1)] - E[X_h(1)]| with MC
 M = 1000
 def φ(x): return x # The test function in question
 expectation_X_1 = exp(μ)
-plt.loglog(H, [h for h in H], label = 'Reference slope h')
-plt.loglog(H, [abs(expectation_X_1 - sum(φ(last(X_h(gen_W_10()[1], i))) for _ in range(M)) / M)
-    for i in range(1, 11)])
+plt.loglog(H, [h for h in H], label = 'Reference slope $h$')
+weak_errors = [abs(expectation_X_1 - sum(φ(last(X_h(gen_W_10()[1], i))) for _ in range(M)) / M)
+    for i in range(1, 11)]
+plt.loglog(H, weak_errors, label = 'Weak error Monte Carlo estimate')
 plt.legend()
-plt.title('Weak error of |E[X(1)] - E[X_h(1)]|')
+plt.title('Weak error of $|E[X(1)] - E[X_h(1)]|$')
 plt.show()
