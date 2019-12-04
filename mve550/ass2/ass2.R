a72aa43685cc902b1be5144f6bdaf2048a0959d1
BotLauri <31406044+BotLauri@users.noreply.github.com>
2019-12-04 09:15:24 +0100

    Update ass2.R

--- mve550/ass2/ass2.R
+++ mve550/ass2/ass2.R
@@ -36,6 +36,31 @@ cat(sprintf("The probability of extinction for the process in question: %f\n",
 		probability_of_extinction$value))
 
 # (d) Use simulation to check result in (c)
+simExtP <- c()
+for (i in 1:1000) {
+  simExtP[i] <- extinctionProbability(1.1) # Varför 1.1?
+}
+hist(simExtP); print(mean(simExtP))
 
+simBranch <- c()
+for (i in 1:100000) {
+  simBranch[i] <- branch(1.1)
+}
+hist(simBranch); print(mean(simBranch))
+  
 # (e) Compute MLE for λ. Probability of extinction using that λ?
 
+
+# 2. Metropolis Hastings
+
+# (a) Plot the data
+D <- read.table("Regressiondata.txt")
+plot(D)
+
+# (b) Log of P(theta*|data)/P(theta|data)
+prior <- function(theta1, theta2, theta3, theta4) {
+  prior <- theta1*rnorm(1, 10, 10^2) * theta2*rgamma(1, 1/2, 1/10) * theta3*runif(1, 0, 1) * theta4*rgamma(1, 2, 2)
+}
+likelihood <- function(x, theta1, theta2, theta3, theta4) {
+  likelihood <- rnorm(theta1 + theta2*sin((x - theta3)*2*pi), theta4^2)
+}
