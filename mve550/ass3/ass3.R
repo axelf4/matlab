79e32468080a6c8976f42b8511100fb4cefe30b4
Axel Forsman <axelsfor@gmail.com>
2019-12-15 18:34:04 +0100

    Add exercise 1.(c) on assignment 3

--- mve550/ass3/ass3.R
+++ mve550/ass3/ass3.R
@@ -4,3 +4,9 @@ lambda = 36
 sizeA <- 0.4^2; sizeD <- 0.2^2; sizeB <- sizeA - sizeD
 cat(sprintf("The probability of exactly 4 trees in the two squares simultaneously\n\t= %f\n",
 		sum(sapply(0:4, function(k) dpois(k, lambda * sizeD) * dpois(4 - k, lambda * sizeB)^2))))
+
+# (c) Plot simulated trees
+N <- rpois(1, lambda * 1^2) # Number of points
+# Conditioning on n, the location of points are uniformly distributed
+plot(x = runif(N, 0, 1), y = runif(N, 0, 1),
+	pch = 2, xlim = c(0, 1), ylim = c(0, 1))
