cf9d7790316a63d35545b89a3f47da557f13e079
Axel Forsman <axelsfor@gmail.com>
2021-04-22 17:22:33 +0200

    Add draft of sustainable development assignment

---
+++ mve346/sustainable-dev/flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; let
+            latex = texlive.combine {
+              inherit (texlive) scheme-basic latexmk
+                babel-swedish biblatex biber
+                collection-fontsrecommended microtype crimson xkeyval fontaxes
+                siunitx
+                booktabs
+              ;
+            };
+          in [
+            latex
+          ];
+        };
+      });
+}
